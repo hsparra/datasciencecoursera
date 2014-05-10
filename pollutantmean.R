2e6a475bc3c604ca9fd2d335a6c635212c802a82
Hunt Sparra <hsparra.ds@gmail.com>
2014-05-09 20:50:21 -0400

    Read files and compute mean of pollutant
    Create function to read files from list and concatenate results into a
    single data frame.
    Select out all non-NA instances of the passed in pollutant and return
    mean.

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,4 +1,19 @@
 
+df.from.csv.files <- function(file_list) {
+  for (file in file_list) {
+
+    if (!exists("dataset")) {
+      dataset <- read.csv(file)
+    } else {
+      temp.ds <- read.csv(file)
+      dataset <- rbind(dataset, temp.ds)
+      rm(temp.ds)  
+    }
+  }
+
+  return(dataset)
+}
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
@@ -18,10 +33,15 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   
   # Add directory before file so will read in correct location
   files.to.get <- paste(directory, "/", files.to.get, sep="")
-  print(files.to.get)
-  # Read in files - apply read.csv function to each file name
-  #pollutant.data <- sapply(files.to.get, read.csv)
 
-  #
+  # Read into files and Combine data into a single dataframe
+  all.data <- df.from.csv.files(files.to.get)
   
+  # remove rows with an na for the selected pollutant
+  pollutant.of.interest <- all.data[pollutant]
+  noNAs <- pollutant.of.interest[!is.na(pollutant.of.interest)]
+  
+  # Compute mean on passed pollutant
+  mean(noNAs)
+
 }
