76dff3b96a519dda28f93433b4e5081d9333e676
Hunt Sparra <hsparra.ds@gmail.com>
2014-05-12 15:52:11 -0400

    Read files, check threshold and Compute
    Correlation summary is slightly off from expected. Needs more work.

--- r_programming/corr.R
+++ r_programming/corr.R
@@ -8,4 +8,26 @@ corr <- function(directory, threshold = 0) {
     ## nitrate and sulfate; the default is 0
     
     ## Return a numeric vector of correlations
+    
+    # Get files in passed directory
+    file.list <- list.files(directory, full.names=TRUE)
+    
+    # Create vector to return, make full size so allocate once. Can return subset greater than threshold.
+    # Read each file in list
+    # For each file determine if number of complete cases > passed threshold
+    # If greater than threashold compute correlation removing NAs and save for return
+     
+    cors <- numeric(length(file.list))
+    i <- 0
+    
+    for (file in file.list) {
+        temp.ds <- read.csv(file)
+        if (sum(complete.cases(temp.ds)) >= threshold) {
+            i <- i + 1
+            cors[i] <- cor(temp.ds$sulfate, temp.ds$nitrate, use="complete")            
+        }
+    }
+    
+    cors.to.return <- cors[1:i]
+    cors.to.return
 }
