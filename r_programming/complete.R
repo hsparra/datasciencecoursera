8054e3de2510bfaf8d1a97fc4b575c1a5c8134fa
Hunt Sparra <hsparra.ds@gmail.com>
2014-05-12 10:59:40 -0400

    Create file list with directory from passed range
    Take the passed integer range and directory input and create a vector
    with the file path from the executing script.

--- r_programming/complete.R
+++ r_programming/complete.R
@@ -12,5 +12,10 @@ complete <- function(directory, id = 1:332) {
   ## ...
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
-
+  
+  # Create list of files from id and pad with leading zeroes if needed
+  files.to.get <- paste(sprintf("%03d", id), ".csv", sep="")
+  
+  # Add directory before file so will read in correct location
+  files.to.get <- paste(directory, "/", files.to.get, sep="")
 }
