f0239edfbc204595898d2a876ad15f8c71abfb17
Hunt Sparra <hsparra.ds@gmail.com>
2014-05-12 12:14:00 -0400

    Read file and sum complete observations
    Performing all in-line since not sure how splitting out into separate
    functions would effect auto-grading.

--- r_programming/complete.R
+++ r_programming/complete.R
@@ -18,4 +18,27 @@ complete <- function(directory, id = 1:332) {
   
   # Add directory before file so will read in correct location
   files.to.get <- paste(directory, "/", files.to.get, sep="")
+
+  
+  # Preallocate dataframe so more efficient. Make each column numeric.
+  # Base size on number of files to read since each file only contains data for one site/id
+  # Note: not really important for this assignment but doing for experience
+
+  # Split out length computation so more clear for others
+  len <- length(files.to.get)
+  num.of.obs.by.site <- data.frame(id=numeric(len), nobs=numeric(len))
+  # Create variable to use to keep track of which row working with in file list
+  i = 0
+  
+  # Read into files, as record number of full observations for each site
+  for (file in files.to.get) {
+    temp.ds <- read.csv(file)
+    i = i + 1
+    # Used passed site for id, could use the id value from the first data row from file which would be more error proof
+    num.of.obs.by.site$id[i] <- id[i]
+    # Count the number of complete observations for the site
+    num.of.obs.by.site$nobs[i] <- sum(complete.cases(temp.ds))
+    rm(temp.ds)  
+  }
+  num.of.obs.by.site
 }
