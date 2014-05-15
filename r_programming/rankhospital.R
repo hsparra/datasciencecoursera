6d0468a74f7f8b12ecb059412be95d69b2c637db
Hunt Sparra <hsparra.ds@gmail.com>
2014-05-15 11:26:29 -0400

    Rankhospital and update column assignment
    Add rankhospital function. Did not factor out commonality with with
    best function in order to simplify submission for grading.

--- r_programming/rankhospital.R
+++ r_programming/rankhospital.R
@@ -1,6 +1,6 @@
 library(datasets)
 
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num="best") {
     
     # validate state
     if (!(state %in% state.abb)) {
@@ -18,18 +18,41 @@ best <- function(state, outcome) {
         stop("invalid outcome")
     }
     
+    # validate ranking
+    if (num == "best") {
+        rank <- 1
+    } else if (num == "worst") {
+        rank <- 0
+    } else if (is.numeric(num)) {
+        rank <- num
+    } else {
+        stop("invalid rank")
+    }
+    
     #read outcome data
     outcomes <- read.csv("outcome-of-care-measures.csv", na.strings=c("NA","Not Available"), stringsAsFactors=FALSE)
     
     # set column of interest to numeric
     outcomes[,col] <- as.numeric(outcomes[,col])
-
-    #remove NAs from outcomes, keep Hosp
+    
+    # remove NAs from outcomes, keep Hosp
     good <- outcomes[!is.na(outcomes[,col]) & outcomes[,7] == state,c(2,col)]
     
+    # order hospitals with by lowest rate in alpha orde within rate
+    ordered <- good[order(good[2], good[1]),]
+    
     # return hospital(s) with lowest 30-day death rate
-    best.hosp <- good[good[,2] == min(good[,2]),1]
+    #best.hosp <- good[good[,2] == min(good[,2]),1]
     
     # sort output and return lowest value
-    best.hosp[order(best.hosp)[1]]
+    #best.hosp[order(best.hosp)[1]]
+    
+    # return selected rank
+    if (num == "worst") {
+        selected <- ordered[nrow(ordered),1]
+    } else {
+        selected <- ordered [rank,1]
+    }
+    
+    selected
 }
