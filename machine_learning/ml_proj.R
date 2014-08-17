a2a734a56ba9be6846657feb9c30d378b537a5c4
Hunt Sparra <hsparra.ds@gmail.com>
2014-08-17 13:35:35 -0400

    Multiple Version of removal based on Corrrelation
    Contains multiple versions of determining which variables to remove
    based on correlation with either class or with other variables. Still
    need to pick on for final model.

--- machine_learning/ml_proj.R
+++ machine_learning/ml_proj.R
@@ -11,16 +11,16 @@ inTrain <- createDataPartition(y=data$classe, p=0.7, list=FALSE)
 train <- data[inTrain,]
 test <- data[-inTrain,]
 
-set.seed(210)
+## Select subset for initial testing so does not take so long to iterate
+set.seed(260)
 train.sm <- train[sample(nrow(train), size=2000, replace=FALSE),]
 
-# Check for zero covariates
+# Check for zero covariates and remove
 nsv2 <- nearZeroVar(train.sm, saveMetrics = TRUE)
 nsv <- nearZeroVar(train.sm)
-
 train.sm <- train.sm[,-nsv]
 
-# function to check for excessive NAs for a variable
+# function to check for excessive NAs for a variable and remove
 naCheck <- function (df, check) {
   y <- character()
   z <- numeric()
@@ -37,10 +37,9 @@ naCheck <- function (df, check) {
 }
 
 lotsNas <- naCheck(train.sm, .95)
-
 train.sm2 <- train.sm[,-lotsNas$colNum]
 
-# Remove non-numeric columns
+# Remove non-numeric columns, Note: could convert instead
 colsOfType <- function (df, type) {
   x <- integer()
   for (i in 1:length(df)) {
@@ -51,31 +50,35 @@ colsOfType <- function (df, type) {
 }
 
 colsToRemove <- colsOfType(train.sm2, "integer")
-
 train.sm2 <- train.sm2[,-colsToRemove]
 
 # make resonse a factor
 train.sm2$classe <- as.factor(train.sm2$classe)
 
 # Look for high correlations and remove
-m <- abs(cor(finalTrain[,-28]))
+m <- abs(cor(train.sm2[,-28]))
+ft <- train.sm2
+ft$classe <- as.numeric(ft$classe)
+m <- abs(cor(ft))   # Seems results not as good, but close. 
+
 diag(m) <- 0
 which(m > .8, arr.ind=T)
-#train.sm2 <- train.sm2[, -c("yaw_belt", "gyros_arm_x")]
-# find variables with correlation < 0.05 with classe
-rowsLittleCor <- row.names(m)[m[,dim(m)[2]] < .05]
-train.sm3 <- train.sm2[,!(colnames(train.sm2) %in% rowsLittleCor)]
+## Note: reduced accuracy of results
+#train.sm2 <- train.sm2[, -which(names(train.sm) %in% c("yaw_belt", "gyros_arm_x"))]
+
+# find variables with correlation < 0.05 with classe and remove
+## Keep - results in better prediction on "test" dataset
+## TODO
+#   - Try > .8
+#   - switch back to using the original m and < .05  # Predicted 92.x% on test dataset
+#rowsLittleCor <- row.names(m)[m[,dim(m)[2]] < .02]  # Predicted 92.3% on test dataset
+rowsLittleCor <- row.names(m)[m[,dim(m)[2]] > .8]   # Predicted 92.7 % of test dataset
+train.sm3 <- train.sm2[,!(colnames(train.sm2) %in% rowsLittleCor[-length(rowsLittleCor)])]
 
 finalTrain <- train.sm2
-# try PCA
-# TODO - not current working
-modelPCA <- train(classe ~ ., method="glm", preProcess="pca", data=finalTrain)
-confusionMatrix(test$classe, predict(modelPCA, test))
+finalTrain <- train.sm3
 
-# Try Tree
-modTree <- train(classe ~ ., method="rpart", data=finalTrain)
-#modTree <- train(x=answers, y=train.sm,method="rpart")
-
+## ~~~~~~~~~~~~~~~ Create model ~~~~~~~~~~~~~~~~~~ 
 ## The BEST
 ## TODO - move up higher on final
 # Random Forest 
@@ -90,7 +93,20 @@ test.sm <- test.sm[,-colsToRemove]
 pred <- predict(modFit, test.sm); 
 test.sm$predRight <- pred == test.sm$classe
 table(pred, test.sm$classe)
+sum(test.sm$predRight)/length(test.sm$predRight)
 qplot(pred, test.sm$classe, colour=test.sm$classe) + geom_jitter()
+
+
+## ~~~~~~~ The rest ~~~~~~~~
+# try PCA
+# TODO - not current working
+modelPCA <- train(classe ~ ., method="glm", preProcess="pca", data=finalTrain)
+confusionMatrix(test$classe, predict(modelPCA, test))
+
+# Try Tree
+modTree <- train(classe ~ ., method="rpart", data=finalTrain)
+#modTree <- train(x=answers, y=train.sm,method="rpart")
+
 
 
 ## Try using Boosting
@@ -99,7 +115,23 @@ qplot(predict(modBoost, test.sm), classe, data=test.sm)
 
 
 ## Linear Model - does not work for classification
-modLmAll <- train(classe ~ ., data=finalTrain, method="lm")
+modLmAll <- train(as.numeric(classe) ~ ., data=finalTrain, method="lm")
+
+## Linear model using only highest correclated vars
+modLmHigh <- train(as.numeric(classe) ~  magnet_dumbbell_z + roll_forearm + roll_arm + yaw_arm + 
+                     roll_belt + magnet_forearm_y + pitch_dumbbell, data=finalTrain, method="lm")
+modLmHigh <- train(as.numeric(classe) ~  magnet_dumbbell_z + roll_belt + yaw_belt + 
+                     gyros_belt_y + 
+                     pitch_arm + roll_dumbbell + pitch_dumbbell + yaw_dumbbell +
+                     gyros_dumbbell_y + gyros_dumbbell_z + 
+                     roll_forearm + pitch_forearm +
+                     roll_arm + gyros_arm_x + magnet_forearm_y,
+                   data=finalTrain, method="lm")
+                   
+
+modLmHigh2 <- lm(as.numeric(classe) ~  magnet_dumbbell_z + roll_forearm + roll_arm + 
+                     roll_belt + magnet_forearm_y, data=finalTrain)
+
 
 ## lda and naive Bayes - not near Random Forest
 modlda <- train(classe ~ ., data=finalTrain, method="lda")
