ac04dce12c7b79b404e40970c2aa49e4aad751ca
Hunt Sparra <hsparra.ds@gmail.com>
2014-08-19 20:23:23 -0400

    Basic Graph with chosen variables and coloring

--- data_products/mpg_comparison/server.R
+++ data_products/mpg_comparison/server.R
@@ -1,25 +1,39 @@
 library(shiny)
 library(ggplot2)
 
-data2 <- read.csv("data/electric_cars_addl2.csv",header=T,stringsAsFactors=F)
+data2 <- read.csv("data/electric_cars_addl2.csv",header=T,stringsAsFactors=T)
+data2$year <- as.factor(data2$year)
+d <- list(Range = "range",
+          MPGe = "combined",
+          Weight = "lbs",
+          Efficience = "kwh.p.100mi",
+          Battery = "kwh.t")
 
 shinyServer(
   function(input, output, session) {
     # Combine the selected variables into a new data frame
     selectedData <- reactive({
-      data2[, c(input$x, input$y)]
+      if (input$recent == FALSE)
+        data2
+      else
+        data2[data2$year %in% c("2009","2010","2011","2012","2013","2014"),]
     })
-    output$text1 <- renderPrint({input$x})
-    output$graph <- reactivePlot(function() {
-      p <- ggplot(selectedData(), aes_string(x=input$x, y=input$y)) + geom_point()
-      print(p)
+
+    output$plot1 <- renderPlot({
+      
+      p <- ggplot(selectedData(), aes_string(x=input$x, y=input$y)) + 
+        geom_point(aes_string(size=3, color=input$color)) 
+#       + geom_text(aes(vjust=-2, label=model)) 
+       if (input$names == TRUE)
+         p <- p + geom_text(aes(vjust=-2, label=model)) 
+      p #print(p)
     })
-#     
+    
 #   output$xSelect <- renderUI({
-#     selectInput("x","X Axis",names(data))
+#     selectInput("x","X Axis",names(d),selected="Range")
 #   })
 #   output$ySelect <- renderUI({
-#     selectInput("y","Y Axis",names(data))
+#     selectInput("z","Y Axis 2",names(d),selected="Weight")
 #   })
 #   output$colorSelect <- renderUI({
 #     selectInput("c","Variable To Color With",names(data))
--- data_products/mpg_comparison/ui.R
+++ data_products/mpg_comparison/ui.R
@@ -2,20 +2,42 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel("EV Explorer"),
-  sidebarLayout(
-    sidebarPanel(
-      
-#       htmlOutput("xSelect"),
-#       htmlOutput("ySelect"),
-#       htmlOutput("colorSelect"),
-#       submitButton("Submit")
-      selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs"),selected="range"),
-      selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs"), selected="lbs"),
-      submitButton("Submit")
-      ),
-    mainPanel(
-      plotOutput("graph")
-      )
+  #titlePanel("EV Explorer"),
+  title = "EV Explorer",
+  h2("EV Explorer"),
+  p("Use this app to compare different characteristics of some recent and no recent electric vehicles."),
+  plotOutput("plot1"),
+  fluidRow(
+    column(3, 
+           selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"), selected="range")
+           ),
+   column(3, 
+          selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"),selected="lbs")
+          ),
+   column(3,
+          selectInput("color","Color", c("year","make","class"),selected="year"),
+          checkboxInput("names",label="Show Model Names"),
+          checkboxInput("recent",label="Show Only Recent")
+          )
     )
+  
+#   sidebarLayout(
+#     sidebarPanel(
+#       
+# #       htmlOutput("ySelect"),
+# #       htmlOutput("xSelect"),
+#       
+# #       htmlOutput("colorSelect"),
+# #       submitButton("Submit")
+# #       h4("Selected X:"),verbatimTextOutput("text1")
+#       selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"), selected="lbs"),
+#       selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"),selected="range"),
+#       selectInput("color","Color", c("year","make","class"),selected="year"),
+#       checkboxInput("names",label="Show Model Names")
+# #       submitButton("Submit")
+#       ),
+#     mainPanel(
+#       plotOutput("plot1")
+#       )
+#     )
   ))
