0f914275ad39a22fd8886b7d64991ace09b81f44
Hunt Sparra <hsparra.ds@gmail.com>
2014-08-21 18:08:09 -0400

    Change to compare fuel costs
    Experiment with showing comparative fuel costs based on user input

--- data_products/mpg_comparison/server.R
+++ data_products/mpg_comparison/server.R
@@ -1,44 +1,61 @@
 library(shiny)
 library(ggplot2)
+source("epa_helpers.R")
 
-data2 <- read.csv("data/electric_cars_addl2.csv",header=T,stringsAsFactors=T)
-data2$year <- as.factor(data2$year)
-d <- list(Range = "range",
-          MPGe = "combined",
-          Weight = "lbs",
-          Efficience = "kwh.p.100mi",
-          Battery = "kwh.t")
+epa <- read.csv("data/EPA_EV_Data.csv",header=T,stringsAsFactors=F)
+epa$year <- as.factor(epa$Year)
+
+
 
 shinyServer(
   function(input, output, session) {
     # Combine the selected variables into a new data frame
-    selectedData <- reactive({
-      if (input$recent == FALSE)
-        data2
-      else
-        data2[data2$year %in% c("2009","2010","2011","2012","2013","2014"),]
-    })
-
-    output$plot1 <- renderPlot({
-      
-      p <- ggplot(selectedData(), aes_string(x=input$x, y=input$y)) + 
-        geom_point(aes_string(size=3, color=input$color)) 
-#       + geom_text(aes(vjust=-2, label=model)) 
-       if (input$names == TRUE)
-         p <- p + geom_text(aes(vjust=-2, label=model)) 
-      p #print(p)
+#     selectedData <- reactive({
+#       if (input$recent == FALSE)
+#         data2
+#       else
+#         data2[data2$year %in% c("2009","2010","2011","2012","2013","2014"),]
+#     })
+# 
+#     output$plot1 <- renderPlot({
+#       
+#       p <- ggplot(selectedData(), aes_string(x=input$x, y=input$y)) + 
+#         geom_point(aes_string(size=3, color=input$color)) 
+# #       + geom_text(aes(vjust=-2, label=model)) 
+#        if (input$names == TRUE)
+#          p <- p + geom_text(aes(vjust=-2, label=model)) 
+#       p #print(p)
+#     })
+    
+    observe({
+      models <- epa[epa$Make==input$makeSel,"Model"]
+      models2 <- epa[epa$Make==input$makeSel2,"Model"] 
+      updateSelectInput(session, "makeSel", choices = sort(unique(epa$Make)),
+                        selected=input$makeSel)
+      updateSelectInput(session, "makeSel2", choices = sort(unique(epa$Make)),
+                        selected=input$makeSel2)
+      updateSelectInput(session, "modelSel", choices = sort(models))
+      updateSelectInput(session, "modelSel2", choices = sort(models2))
     })
     
-#   output$xSelect <- renderUI({
-#     selectInput("x","X Axis",names(d),selected="Range")
-#   })
-#   output$ySelect <- renderUI({
-#     selectInput("z","Y Axis 2",names(d),selected="Weight")
-#   })
-#   output$colorSelect <- renderUI({
-#     selectInput("c","Variable To Color With",names(data))
-#   })
-
-  
+    
+    output$plot1 <- renderPlot({
+      mods <- character()
+      costs <- numeric()
+      if (input$modelSel > ""){
+        mods <- c(input$modelSel)
+        epa_data <- epa[epa$Model == input$modelSel,]
+        costs <- c(compCost(epa_data, as.integer(input$daily_com)))
+      }
+      if (input$modelSel2 > "") {
+        mods <- c(mods, input$modelSel2)
+        epa_data <- epa[epa$Model == input$modelSel2,]
+        costs <- c(costs, Cost=compCost(epa_data, as.integer(input$daily_com)))
+      }
+      
+      out <- data.frame(Model=mods, Cost=costs)
+      ggplot(out, aes(x=Model, y=Cost, fill=Model)) + geom_bar(stat="identity") + 
+        geom_text(aes(label=sprintf("$ %3.2f",Cost), vjust=-0.2))
+    })
   
 })
--- data_products/mpg_comparison/ui.R
+++ data_products/mpg_comparison/ui.R
@@ -5,39 +5,47 @@ shinyUI(fluidPage(
   #titlePanel("EV Explorer"),
   title = "EV Explorer",
   h2("EV Explorer"),
-  p("Use this app to compare different characteristics of some recent and no recent electric vehicles."),
-  plotOutput("plot1"),
-  fluidRow(
-    column(3, 
-           selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"), selected="range")
-           ),
-   column(3, 
-          selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"),selected="lbs")
-          ),
-   column(3,
-          selectInput("color","Color", c("year","make","class"),selected="year"),
-          checkboxInput("names",label="Show Model Names"),
-          checkboxInput("recent",label="Show Only Recent")
-          )
-    )
+#   p("Use this app to compare different characteristics of some recent and no recent electric vehicles."),
+#   plotOutput("plot1"),
+#   fluidRow(
+#     column(3, 
+#            selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"), selected="range")
+#            ),
+#    column(3, 
+#           selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"),selected="lbs")
+#           ),
+#    column(3,
+#           selectInput("color","Color", c("year","make","class"),selected="year"),
+#           checkboxInput("names",label="Show Model Names"),
+#           checkboxInput("recent",label="Show Only Recent")
+#           )
+#     )
   
-#   sidebarLayout(
-#     sidebarPanel(
-#       
-# #       htmlOutput("ySelect"),
-# #       htmlOutput("xSelect"),
-#       
-# #       htmlOutput("colorSelect"),
-# #       submitButton("Submit")
-# #       h4("Selected X:"),verbatimTextOutput("text1")
+  sidebarLayout(
+    sidebarPanel(
+      
+#       htmlOutput("make"),
+#       htmlOutput("model")
+      h4("First Car to Compare:"),
+      selectInput("makeSel","Make",""),
+      selectInput("modelSel","Model",""),
+      h4("Second Car to Compare:"),
+      selectInput("makeSel2","Make",""),
+      selectInput("modelSel2","Model",""),
+      sliderInput("daily_com","Avg Weekday Comute",
+                  min=1, max=100, value=40)
+      
+#       htmlOutput("colorSelect"),
+#       submitButton("Submit")
+#       h4("Selected X:"),verbatimTextOutput("text1")
 #       selectInput("y", "Y", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"), selected="lbs"),
 #       selectInput("x", "X", c("range","combined","kwh.t","kwh.p.100mi","lbs","year"),selected="range"),
 #       selectInput("color","Color", c("year","make","class"),selected="year"),
 #       checkboxInput("names",label="Show Model Names")
-# #       submitButton("Submit")
-#       ),
-#     mainPanel(
-#       plotOutput("plot1")
-#       )
-#     )
+#       submitButton("Submit")
+      ),
+    mainPanel(
+      plotOutput("plot1")
+      )
+    )
   ))
