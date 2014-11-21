e13555e9659276b3f0844b5c872335c4983c855c
Hunt Sparra <hsparra.ds@gmail.com>
2014-11-21 14:54:50 -0500

    Change matching to perform backoff
    Depending on the type of matching table a back is performed if no match
    is found on the more stringent bases (i.e. more words).

--- Capstone/shinyApp/predictor.R
+++ Capstone/shinyApp/predictor.R
@@ -19,13 +19,13 @@ cleanTextForMatch <- function(str) {
     str
 }
 
-decodeTopMatches <- function(matches, dict, n=3) {
+decodeTopMatches <- function(matchesIn, dict, n=3) {
+    matches <- as.vector(as.matrix(matchesIn))
     if (length(matches) < n) {
         n <- length(matches)
     }
     top <- character(0)
     for (i in 1:n) {
-        #         cat("looking for decode of:", matches[i], "  which =", dict[matches[i]], "\n")   # TEST
         top <- append(top, dict[matches[i]])
     }
     top
@@ -34,17 +34,36 @@ decodeTopMatches <- function(matches, dict, n=3) {
 findMatches <- function(str, t, dict, n=3) {
     
     l <- findAllMatches(str, t, dict, n)
-    l[[1]]
+    l[[1]]     # First entry are the top matches
+}
+
+buildMatchString <- function(x, dict, lvl=2) {
+    vars <- x[(length(x) - lvl + 1) : length(x)]
+    vars <- gsub(" ", "", vars)
+    vars <- match(vars, dict)
+    paste("V", 1:lvl,"==",vars, sep="", collapse=" & ")
 }
 
 findAllMatches <- function(str, t, dict, n=3) {
-    x <- cleanText(str)
-    key <- match(x[length(x)], dict)
-    if (!is.na(key)) {
-        matches <- t[w1 == key]
-        matches <- matches[order(-count)]
+    x <- cleanTextForMatch(str)
+    lngth <- dim(t)[2] - 2
+
+    cat("length is", lngth, "\n")   # TEST
+
+    for (i in lngth:1) {
+        key <- buildMatchString(x, dict, i)
+        cat("search with key", key, "\n")    #TEST
+        matches <- eval(parse(text=paste("t[", key, "]")))
+
+        if (dim(matches)[1] > 0) {
+            matches <- matches[order(-count)]
+            break
+        }
     }
+    
+
+    if (is.null(matches)) { return(list())}
     # decode values
-    list(decodeTopMatches(matches$w2, dict, n), matches)
+    list(decodeTopMatches(matches[, (i+1), with=FALSE], dict, n), matches)
 }
 
