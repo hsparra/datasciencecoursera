2bf341a7a5e493cd9cb7309e442f38384ff67466
Hunt Sparra <hsparra.ds@gmail.com>
2014-12-08 14:04:46 -0500

    Add new 4gram matching and CleanText functions
    Kept old functions under different names.
    CleanText function more closely match function used when cleaning the
    original data. Does not have option to exclude last word.

--- Capstone/shinyApp/predictor.R
+++ Capstone/shinyApp/predictor.R
@@ -22,7 +22,7 @@ cleanTextForMatch <- function(str) {
     str
 }
 
-cleanText <- function(data, split=FALSE) {
+cleanText_prior <- function(data, split=FALSE) {
     data <- tolower(data)
     data <- unlist(strsplit(data, split=" "))
     data <- remove_stopwords(data, stopwords())
@@ -43,9 +43,63 @@ cleanText <- function(data, split=FALSE) {
     data
 }
 
+cleanText <- function(data, split=FALSE, stemWords=TRUE) {
+    data <- unlist(strsplit(data, split=" "))
+    data <- tolower(data)
+    data <- gsub("’", "'", data)   # replace right single quote mark with chracter used in stopwords()
+    data <- remove_stopwords(data, stopwords())
+    data <- gsub("[[:punct:]]", " ", data)
+    data <- gsub("[^a-z]", " ", data)
+    data <- unlist(strsplit(data, split=" "))
+    data <- data[ data != " "]
+
+    if (length(data) <= 1) {
+        return("")
+    }
+    
+    if (stemWords) {
+        data <- wordStem(data)
+    }
+    data <- remove_stopwords(data, stopwords())   # Remove stopwords created by stemming
+    
+    
+    data <- gsub("(?<=[^[se]])s$", "", data, perl = TRUE)    # Remove at end of word if not preceded by an s
+    data <- data[data != ""]
+#     if (length(data) < 4) {
+#         return("")
+#     }
+    if (split) {
+        data <- paste(data, collapse = " ")
+    } else {
+        data <- unlist(strsplit(data, split = " ")) %>%
+            function (x) x[ x != ""]
+    }
+    
+    data <- gsub("[ ]{2,}", " ", data)
+    data <- gsub("^[ ]+", "", data)
+    data
+}
+
 
 q4Match <- function(inText, matchTable, wrds, maxToReturn=1) {
     x <- cleanText(inText)
+    n <- length(x)
+    z <- matchTable[V3 == match(x[n], wrds) ]
+    z1 <- z[V2 == match(x[(n-1)], wrds) ]
+    if (dim(z1)[1] > 0) {
+        z <- z1
+    }
+    z1 <- z[V1 ==match(x[(n-2)], wrds) ]
+    if (dim(z1)[1] > 0) {
+        z <- z1
+    }
+    z <- z[order(-logpAll, -logpV4, -count)]
+    pred <- wrds[z$V4[1:maxToReturn]] %>% na.omit
+    pred
+}
+
+q4Match_all <- function(inText, matchTable, wrds, maxToReturn=1) {
+    x <- cleanText(inText, noStemLast = FALSE)
     z <- matchTable[V3 == match(x, wrds)]
     z1 <- z[V2 == match(x, wrds)]
     if (dim(z1)[1] > 0) {
@@ -55,7 +109,7 @@ q4Match <- function(inText, matchTable, wrds, maxToReturn=1) {
     if (dim(z1)[1] > 0) {
         z <- z1
     }
-    z <- z[order(-ratio2, -ratio, -logp)]
+    z <- z[order(-logpAll, -logpV4, -count)]
     pred <- wrds[z$V4[1:maxToReturn]] %>% na.omit
     pred
 }
