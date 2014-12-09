c608d1442730ceb28419db4bea2b9ab5b90124d4
Hunt Sparra <hsparra.ds@gmail.com>
2014-12-09 16:13:38 -0500

    Add back matching with new tables

--- Capstone/shinyApp/predictor.R
+++ Capstone/shinyApp/predictor.R
@@ -3,11 +3,11 @@ require(tm)
 require(tau)
 require(magrittr)
 
-cleanTextForMatch<- function(data) {
+cleanTextForMatch2<- function(data) {
     data <- unlist(strsplit(data, split=" "))
     data <- tolower(data)
     data <- gsub("’", "'", data)   # replace right single quote mark with chracter used in stopwords()
-    data <- remove_stopwords(data, stopwords())
+#     data <- remove_stopwords(data, stopwords())
     data <- gsub("[[:punct:]]", " ", data)
     data <- gsub("[^a-z]", " ", data)
     data <- unlist(strsplit(data, split=" "))
@@ -17,8 +17,8 @@ cleanTextForMatch<- function(data) {
         return("")
     }
     
-    data <- wordStem(data)
-    data <- remove_stopwords(data, stopwords())   # Remove stopwords created by stemming
+#     data <- wordStem(data)
+#     data <- remove_stopwords(data, stopwords())   # Remove stopwords created by stemming
 
     data <- gsub("(?<=[^[se]])s$", "", data, perl = TRUE)    # Remove at end of word if not preceded by an s
     data <- data[data != ""]
@@ -31,6 +31,27 @@ cleanTextForMatch<- function(data) {
 #     }
     
     data <- gsub("[ ]{2,}", " ", data)
+    data <- gsub("^[ ]+", "", data)
+    unlist(data)
+}
+
+cleanTextForMatch <- function(data) {
+    data <- unlist(strsplit(data, split=" "))
+    
+    data <- tolower(data)
+    data <- gsub("’", "'", data)   # replace right single quote mark with chracter used in stopwords()
+    
+    data <- gsub("[^a-z]", " ", data)
+    data <- unlist(strsplit(data, split=" "))
+    data <- data[ data != " "]
+    if (length(data) <= 1) {
+        return("")
+    }
+    
+    data <- data[data != ""]
+    data <- unlist(strsplit(data, split = " ")) %>%
+        function (x) x[ x != ""]
+
     data <- gsub("^[ ]+", "", data)
     data
 }
@@ -51,5 +72,66 @@ q4Match <- function(inText, matchTable, wrds, maxToReturn=1) {
     z <- z[order(-logpAll, -logpV4, -count)]
     pred <- wrds[z$V4[1:maxToReturn]] %>% na.omit
     pred
+}
+
+selectReturn <- function(m, wrds, numberToReturn = 1) {
+    m <- m[order(-ratio)]
+}
+
+match2Gram <- function(x, m2gram) {
+    m <- m2gram[V1 == last(x)]
+}
+
+match3Gram <- function(x, m3gram) {
+    n <- length(x)
+    m <- m3gram[V2 == x[n]]
+    if (dim(m)[1] == 0){
+        return(m)
+    }
+    m <- m[V1 == x[(n-1)]]
+}
+
+match4Gram <- function(x, m4gram) {
+    n <- length(x)
+    m <- m4gram[V3 == x[n]]
+    if (dim(m)[1] == 0){
+        return(m)
+    }
+    m <- m[V2 == x[(n-1)]]
+    if (dim(m)[1] == 0){
+        return(m)
+    }
+    m <- m[V1 == x[(n-2)]]
+}
+
+qMatch <- function(inText, m4gram, m3gram, m2gram, wrds, numberToReturn = 1) {
+    x <- cleanTextForMatch(inText)
+    x <- match(x, wrds)
+    if (length(x) == 0) {
+        return("")
+    }
+    m <- data.table()
+    if (length(x) >= 3) {
+        m <- match4Gram(x, m4gram)
+        if (dim(m)[1] > 0) {
+            m <- m[order(-ratio)]
+            return( wrds[m$V4[1:numberToReturn]])
+        }
+    }
+    
+    if (length(x) >= 2) {
+        m <- match3Gram(x, m3gram)
+        if (dim(m)[1] > 0) {
+            m <- m[order(-ratio)]
+            return( wrds[m$V3[1:numberToReturn]])
+        }
+    }
+    
+    m <- match2Gram(x, m2gram)
+    if (dim(m)[1] > 0) {
+        m <- m[order(-ratio)]
+        return( wrds[m$V3[1:numberToReturn]])
+    }
+    return("oops")
 }
 
