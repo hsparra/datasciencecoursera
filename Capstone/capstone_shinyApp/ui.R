02a7a25651e2e6954832b4ff8fc5384f98d58e01
Hunt Sparra <hsparra.ds@gmail.com>
2014-12-11 15:49:16 -0500

    Switch to having answers in 2 columns below entry

--- Capstone/capstone_shinyApp/ui.R
+++ Capstone/capstone_shinyApp/ui.R
@@ -4,45 +4,45 @@ shinyUI(fluidPage(
     
     titlePanel("Coursera Data Science Capstone"),
 
-    mainPanel(
-            h3("Word Prediciton"),
-            hr(),
-            helpText("Begin typing to see prediction of next word:"),
-            textInput("phrase", "",""),
-            tags$head(tags$style(type="text/css", "#phrase {width: 450px}")),
-
-            br(),
-            hr(),
-            p(em("Predicted next word:")),
-            span(h3(textOutput("guess")), style="color:green"),
-
-            hr(),
-            em(textOutput("oText")),
-            htmlOutput("otherGuesses")            
-        )
-
-#     fluidRow(
-#         column(12,
-#                h3("Word Prediciton"),
-#                hr()
-#                )
-#         ),
-#     
-#     fluidRow(
-#         column(6,
-#                helpText("Begin typing to see prediction of next word:"),
-#                textInput("phrase", "",""),
-#                tags$head(tags$style(type="text/css", "#phrase {width: 450px}"))
-#             ),
-#         column(6,
-#                p(em("Predicted next word:")),
-#                span(h3(textOutput("guess")), style="color:green"),
-#                
-#                hr(),
-#                em(textOutput("oText")),
-#                htmlOutput("otherGuesses")  
-#                )
+#     mainPanel(
+#             h3("Word Prediciton"),
+#             hr(),
+#             helpText("Begin typing to see prediction of next word:"),
+#             textInput("phrase", "",""),
+#             tags$head(tags$style(type="text/css", "#phrase {width: 450px}")),
+# 
+#             br(),
+#             hr(),
+#             p(em("Predicted next word:")),
+#             span(h3(textOutput("guess")), style="color:green"),
+#             
+#             br(),
+#             hr(),
+#             em(textOutput("oText")),
+#             htmlOutput("otherGuesses")            
 #         )
+
+    fluidRow(
+        column(12,
+               h3("Word Prediciton"),
+               hr(),
+               helpText("Begin typing to see prediction of next word:"),
+               textInput("phrase", "",""),
+               tags$head(tags$style(type="text/css", "#phrase {width: 450px}"))
+               )
+        ),
+    
+    fluidRow(
+        column(3,
+               p(em("Predicted next word:")),
+               span(h3(textOutput("guess")), style="color:green")
+            ),
+        column(2,
+               em(textOutput("oText")),
+               br(),
+               htmlOutput("otherGuesses")  
+               )
+        )
     
     
     )
