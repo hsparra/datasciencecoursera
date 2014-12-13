e1bc33f06c71dc3e01bdbd473819aee0fbd03c91
Hunt Sparra <hsparra.ds@gmail.com>
2014-12-13 15:46:36 -0500

    Remove commented out code

--- Capstone/capstone_shinyApp/predictor.R
+++ Capstone/capstone_shinyApp/predictor.R
@@ -6,38 +6,6 @@ require(magrittr)
 singleLettersResultsToRemove <- setdiff(letters, c("a", "i"))
 maxToReview <- 5
 
-cleanTextForMatch2<- function(data) {
-    data <- unlist(strsplit(data, split=" "))
-    data <- tolower(data)
-    data <- gsub("’", "'", data)   # replace right single quote mark with chracter used in stopwords()
-#     data <- remove_stopwords(data, stopwords())
-    data <- gsub("[[:punct:]]", " ", data)
-    data <- gsub("[^a-z]", " ", data)
-    data <- unlist(strsplit(data, split=" "))
-    data <- data[ data != " "]
-
-    if (length(data) <= 1) {
-        return("")
-    }
-    
-#     data <- wordStem(data)
-#     data <- remove_stopwords(data, stopwords())   # Remove stopwords created by stemming
-
-    data <- gsub("(?<=[^[se]])s$", "", data, perl = TRUE)    # Remove at end of word if not preceded by an s
-    data <- data[data != ""]
-
-#     if (split) {
-#         data <- paste(data, collapse = " ")
-#     } else {
-        data <- unlist(strsplit(data, split = " ")) %>%
-            function (x) x[ x != ""]
-#     }
-    
-    data <- gsub("[ ]{2,}", " ", data)
-    data <- gsub("^[ ]+", "", data)
-    unlist(data)
-}
-
 cleanTextForMatch <- function(data) {
     data <- unlist(strsplit(data, split=" "))
     
@@ -59,23 +27,6 @@ cleanTextForMatch <- function(data) {
     data
 }
 
-
-# q4Match <- function(inText, matchTable, wrds, maxToReturn=1) {
-#     x <- cleanTextForMatch(inText)
-#     n <- length(x)
-#     z <- matchTable[V3 == match(x[n], wrds) ]
-#     z1 <- z[V2 == match(x[(n-1)], wrds) ]
-#     if (dim(z1)[1] > 0) {
-#         z <- z1
-#     }
-#     z1 <- z[V1 ==match(x[(n-2)], wrds) ]
-#     if (dim(z1)[1] > 0) {
-#         z <- z1
-#     }
-#     z <- z[order(-logpAll, -logpV4, -count)]
-#     pred <- wrds[z$V4[1:maxToReturn]] %>% na.omit
-#     pred
-# }
 
 selectReturn <- function(m, wrds, maxToReturn = 1) {
     m <- m[order(-ratio)]
@@ -129,7 +80,6 @@ qMatch <- function(inText, m4gram, m3gram, m2gram, wrds, maxToReturn = 1) {
         m <- match4Gram(x, m4gram)
         if (dim(m)[1] > 0) {
             m <- m[order(stopWord, -ratio)]
-#             m <- m[order(-ratio)]
             return( wrds[m$V4[1:maxToReview]] %>% cleanResults(maxToReturn))
         }
     }
@@ -138,7 +88,6 @@ qMatch <- function(inText, m4gram, m3gram, m2gram, wrds, maxToReturn = 1) {
         m <- match3Gram(x, m3gram)
         if (dim(m)[1] > 0) {
             m <- m[order(stopWord, -bi_cnt, -ratio)]
-#             m <- m[order(-bi_cnt, -ratio)]
             return( wrds[m$V3[1:maxToReview]] %>% cleanResults(maxToReturn))
         }
     }
